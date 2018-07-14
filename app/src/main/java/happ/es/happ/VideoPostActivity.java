3f837db99408068e0b358c504c52742af1639aff
marta <martagillopez@gmail.com>
2018-07-14 17:02:25 +0200

    si no responde correctamente a la segunda va a ver el video de nuevo

--- app/src/main/java/happ/es/happ/VideoPostActivity.java
+++ app/src/main/java/happ/es/happ/VideoPostActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import happ.es.model.DeviceModel;
 import happ.es.model.ResponseModel;
@@ -17,20 +18,24 @@ import happ.es.types.NavValoracionDia;
 import happ.es.types.TypeGroup;
 import happ.es.util.ConstantesValoracionDia;
 
-import static android.icu.lang.UCharacter.GraphemeClusterBreak.L;
-import static happ.es.happ.R.id.bienestar;
+import static happ.es.types.TypeGroup.A;
 
 public class VideoPostActivity extends AppCompatActivity {
 
     private HappService happService;
 
     private String id;
+
     private DeviceModel deviceModel;
+
+    private boolean segundaOportunidad;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.segundaOportunidad = false;
+
         setContentView(R.layout.activity_video_post);
        //TEXTVIEW
         TextView myTextViewb = (TextView)findViewById(R.id.preguntaGrupoB);
@@ -47,7 +52,7 @@ public class VideoPostActivity extends AppCompatActivity {
         if (device != null && device.getDeviceModel() != null) {
             deviceModel = device.getDeviceModel();
 
-            if (TypeGroup.A.name().equals(deviceModel.getGroup())) {
+            if (A.name().equals(deviceModel.getGroup())) {
                 myTextViewb.setVisibility(View.GONE);
             } else if (TypeGroup.B.name().equals(deviceModel.getGroup())) {
                 myTextViewa.setVisibility(View.GONE);
@@ -61,8 +66,7 @@ public class VideoPostActivity extends AppCompatActivity {
 
 
     public void continuar(View view) {
-
-
+        String NOportunidad= "Primera";
 
         //respuesta
         EditText myRespuestaVideo = (EditText)findViewById(R.id.respuestaGrupos);
@@ -72,17 +76,56 @@ public class VideoPostActivity extends AppCompatActivity {
         SeekBar myValoracionVideo = (SeekBar) findViewById(R.id.valoracionVideo);
         int videoValue =  myValoracionVideo.getProgress();
 
-        Long videoValue2=new Long( videoValue);
-        // Contestar pregunta y marcarla como vista
-        happService.changeShowVideo(id, videoAnswer, videoValue2);
+        //si la respuesta es correcta envio respuesta y voy  a la actividad
+        if ((videoAnswer.matches(".*HIJ*.") && (TypeGroup.A.name().equals(deviceModel.getGroup())))
+                ||((videoAnswer.matches(".*CARAMEL*.") && (TypeGroup.B.name().equals(deviceModel.getGroup())))))
+        {
+
+            Long videoValue2=new Long( videoValue);
+            // Contestar pregunta y marcarla como vista
+            happService.changeShowVideo(id, videoAnswer, videoValue2);
+
+            // Ir a la actividad
+            Intent intent = new Intent(this, ValoracionDiaActivity.class);
+            intent.putExtra(ConstantesValoracionDia.NAVEGACION, NavValoracionDia.MENU.name());
+            this.finish();
+            startActivity(intent);
+        } else {
+            if (segundaOportunidad) {
+
+                //en caso contrario nueva oportunidad
+                Toast toast1 = Toast.makeText(getApplicationContext(),
+                        "Su respuesta no es correcta.Para seguir deberá ver el video de nuevo y contestar correctamente.", Toast.LENGTH_LONG);
+                toast1.show();
+                // a ver el video de nuevo
+
+                Intent intent = new Intent(this, VideoPreActivity.class);
+                this.finish();
+                startActivity(intent);
+
+
+            } else {
+                segundaOportunidad = true;
+
+                //en caso contrario nueva oportunidad
+                Toast toast1 = Toast.makeText(getApplicationContext(),
+                        "Su respuesta no es correcta.Inténtelo de nuevo.", Toast.LENGTH_SHORT);
+                toast1.show();
+                //segunda oportunidad si es correcto paso si no es correcto vuelvo a ver el video
+            }
+
+        }
 
 
 
-        // Ir a la actividad
-        Intent intent = new Intent(this, ValoracionDiaActivity.class);
-        intent.putExtra(ConstantesValoracionDia.NAVEGACION, NavValoracionDia.MENU.name());
-        this.finish();
-        startActivity(intent);
+
+
+
+
+
+
+
+
     }
 
 }
